4f41b335f80814ef4560abde368e7e2b3e2bba49
Moritz Süssel <suessel.moritz@web.de>
2023-08-01 16:15:56 +0200

    dummy screens
    main menu, analysis board, add drills, practice drills

---
+++ app/src/main/java/com/moritzsus/chessflow/view/PracticeDrillsActivity.java
@@ -0,0 +1,27 @@
+package com.moritzsus.chessflow.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.moritzsus.chessflow.R;
+
+public class PracticeDrillsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice_drills);
+
+        Button backButton = findViewById(R.id.backFromPracticeButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(PracticeDrillsActivity.this, MainActivity.class));
+            }
+        });
+    }
+}
